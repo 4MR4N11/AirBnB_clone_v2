97e1352b3732fdaec763c9534c7576b1804d1847
Khalid El Amrani <amraniikhalid0606@gmail.com>
2023-09-06 18:36:03 +0100

    Mandatory part

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,13 +1,26 @@
 #!/usr/bin/python3
 """Fabric script that distributes an archive to your web servers,
 using the function do_deploy."""
-from fabric.api import run, put, env
+from fabric.api import run, put, env, local
+from datetime import datetime
 import os
 
-env.hosts = [
-    '34.227.89.39',
-    '35.174.184.2'
-]
+env.hosts = ['34.227.89.39', '35.174.184.2']
+
+
+def do_pack():
+    """Function to generate a .tgz archive from web_static folder."""
+    local("mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    file = "versions/web_static_{}.tgz".format(date)
+    print("Packing web_static to {}".format(file))
+    try:
+        local("tar -cvzf {} web_static".format(file))
+        print("web_static packed: {} -> {}Bytes".format(file,
+                                                        os.path.getsize(file)))
+        return file
+    except Exception:
+        return None
 
 
 def do_deploy(archive_path):
@@ -15,12 +28,13 @@ def do_deploy(archive_path):
     if not os.path.exists(archive_path):
         return False
     try:
-        arch_no_ext = archive_path.split("/")[-1].split(".")[0]
+        arch_name = os.path.basename(archive_path)
+        arch_no_ext = os.path.splitext(arch_name)[0]
         put(archive_path, "/tmp/")
         run("mkdir -p /data/web_static/releases/{}".format(arch_no_ext))
-        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
-            .format(arch_no_ext, arch_no_ext))
-        run("rm -rf /tmp/{}.tgz".format(arch_no_ext))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+            .format(arch_name, arch_no_ext))
+        run("rm -rf /tmp/{}".format(arch_name))
         run(("mv /data/web_static/releases/{}/web_static/* " +
             "/data/web_static/releases/{}/").format(arch_no_ext, arch_no_ext))
         run("rm -rf /data/web_static/releases/{}/web_static"
@@ -28,6 +42,8 @@ def do_deploy(archive_path):
         run("rm -rf /data/web_static/current")
         run(("ln -s /data/web_static/releases/{}/ " +
             "/data/web_static/current").format(arch_no_ext))
+        print("New version deployed!")
         return True
     except Exception:
+        print("Deployment failed!")
         return False
