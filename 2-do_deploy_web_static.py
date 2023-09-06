9817f62d35270ce380dcfeb82f0bf5b4023a4b89
Khalid El Amrani <amraniikhalid0606@gmail.com>
2023-09-06 20:26:37 +0100

    Mandatory part

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,33 +1,30 @@
 #!/usr/bin/python3
 """Fabric script that distributes an archive to your web servers,
 using the function do_deploy."""
-from fabric.api import run, put, env
+from fabric.api import *
 import os
 
-env.hosts = ['34.227.89.39', '35.174.184.2']
+env.hosts = ["34.227.89.39", "35.174.184.2"]
 
 
 def do_deploy(archive_path):
     """Function to distribute an archive to web servers."""
+    if not os.path.exists(archive_path):
+        return False
     try:
-        if not os.path.exists(archive_path):
-            return False
-        arch_name = os.path.basename(archive_path)
-        arch_no_ext = os.path.splitext(arch_name)[0]
+        file_names = archive_path.split("/")
+        file_name = file_names[-1].split(".")[0]
         put(archive_path, "/tmp/")
-        run("mkdir -p /data/web_static/releases/{}".format(arch_no_ext))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-            .format(arch_name, arch_no_ext))
-        run("rm -rf /tmp/{}".format(arch_name))
+        run("mkdir -p /data/web_static/releases/{}".format(file_name))
+        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
+            .format(file_name, file_name))
+        run("rm -rf /tmp/{}.tgz".format(file_name))
         run(("mv /data/web_static/releases/{}/web_static/* " +
-            "/data/web_static/releases/{}/").format(arch_no_ext, arch_no_ext))
-        run("rm -rf /data/web_static/releases/{}/web_static"
-            .format(arch_no_ext))
+            "/data/web_static/releases/{}/").format(file_name, file_name))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(file_name))
         run("rm -rf /data/web_static/current")
-        run(("ln -s /data/web_static/releases/{}/ " +
-            "/data/web_static/current").format(arch_no_ext))
-        print("New version deployed!")
+        run(("ln -s /data/web_static/releases/{}/" +
+            " /data/web_static/current").format(file_name))
         return True
     except Exception:
-        print("Deployment failed!")
         return False
