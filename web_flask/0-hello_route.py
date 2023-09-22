2683afd4b9bca88c44c84b5966a28f136d05c33b
Khalid El Amrani <amraniikhalid0606@gmail.com>
2023-09-22 11:54:40 +0100

    Interacting with Flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,14 @@
 from flask import Flask
-
+""" Flask web application """
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
 def hello_route():
+    """ Returns a string at the root route """
     return "Hello HBNB!"
 
 
 if __name__ == "__main__":
+    """runs the application on port 5000"""
     app.run(host="0.0.0.0", port=5000)
