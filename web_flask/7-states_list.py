bad613bafc3d9d22e9557d97a25107c0f6eb71d9
Khalid El Amrani <amraniikhalid0606@gmail.com>
2023-09-22 14:38:13 +0100

    Interacting with Flask

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -16,7 +16,7 @@ def close_storage(exception):
 @app.route("/states_list", strict_slashes=False)
 def list_states():
     data = storage.all(State)
-    return render_template('7-states_list.html', data)
+    return render_template('7-states_list.html', data=data)
 
 
 if __name__ == "__main__":
